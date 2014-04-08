9b695828d5936112c56d4f0f928fdaf1b2f71f97
Peter Penzin <penzin.dev@gmail.com>
2014-04-07 22:09:49 -0700

    Modified IntervalTree.hs

--- src/IntervalTree.hs
+++ src/IntervalTree.hs
@@ -2,20 +2,21 @@ module IntervalTree where
 
 {- Primitives for boundaries and intervals -}
 data Bound a = Incl a | Excl a
-data Interval a = AnInterval {high :: Bound a, low :: Bound a}
+data Interval a = Interval {high :: Bound a, low :: Bound a}
 
 {- Interval tree stores intervals in leafs and branches and allows adding data
  - of any type to each interval. Intervals have to constitute countinuous interval
  - that would span the whole tree
+ - Interval tree is a balanced tree.
  -}
 data IntervalTree a b = ILeaf {value :: b, interval ::Interval a}
    | IBranch { value :: b, left :: IntervalTree a b, right:: IntervalTree a b}
 
 {- Create a tree consisting of a single interval and a value -}
 simpleTree :: (Ord a) => Interval a -> b -> IntervalTree a b
-simpleTree = undefined
+simpleTree int val = ILeaf {value = val, interval = int}
 
-{- Combine two trees together. Intervals they represent have to be adjacent and non-overlapping -}
+{- Combine two trees together perserving tree balance. -}
 combineTrees :: (Ord a) => IntervalTree a b -> IntervalTree a b -> IntervalTree a b 
 combineTrees = undefined
 

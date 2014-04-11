27810b2be57ceeb5f8ef4a14df158f7ce2c96b71
Peter Penzin <penzin.dev@gmail.com>
2014-04-11 16:56:12 -0700

    Minor changes

--- src/IntervalTree.hs
+++ src/IntervalTree.hs
@@ -11,10 +11,15 @@ data Interval a = Interval {high :: Bound a, low :: Bound a}
  -}
 data IntervalTree a b = ILeaf {value :: b, interval ::Interval a}
    | IBranch { value :: b, left :: IntervalTree a b, right:: IntervalTree a b}
+   | INil
 
 {- Create a tree consisting of a single interval and a value -}
 simpleTree :: (Ord a) => Interval a -> b -> IntervalTree a b
 simpleTree int val = ILeaf {value = val, interval = int}
+
+fromList :: (Ord a) => [(Interval a, b)] -> IntervalTree a b
+fromList [] = INil
+fromList (i, x):[] = simpleTree i x
 
 {- Combine two trees together perserving tree balance. -}
 combineTrees :: (Ord a) => IntervalTree a b -> IntervalTree a b -> IntervalTree a b 
